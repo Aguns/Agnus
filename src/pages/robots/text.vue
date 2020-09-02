5a1c726965f28b9833a2700744597f7c6821eab7
Guimarães Mahota <guimaraesmahota@gmail.com>
2020-09-03 00:00:00 +0200

    add text-search

---
+++ src/pages/robots/text.vue
@@ -0,0 +1,35 @@
+<template>
+<section>
+    <v-text-field label="Search" v-model="search"></v-text-field>
+
+    <v-btn primary @click="seachText">Ok</v-btn>
+</section>
+</template>
+
+<script>
+import {
+    robots
+} from "@/services/robots/index.js"
+
+export default {
+    components: {
+
+    },
+    data() {
+        return {
+            search: ""
+        };
+    },
+    computed: {},
+    methods: {
+        async seachText() {
+            const content = {};
+
+            content.searchTerm = this.search;
+
+            robots.text(content);
+        }
+    },
+    fetchOnServer: false
+}
+</script>
---
+++ src/services/robots/index.js
@@ -0,0 +1,9 @@
+import { robot as textRobot}   from '@/services/robots/text.js';
+
+const robots = {
+    text: textRobot
+}
+
+export {
+    robots
+}
